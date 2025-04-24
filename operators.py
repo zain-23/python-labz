7fdda267d987466dac9a184d47131337364c21e4
zain-23 <zaingit780@gmail.com>
2025-04-24 19:47:10 +0500

    Add initial implementation of string examples

---
+++ operators.py
@@ -0,0 +1,10 @@
+one = 1
+two = 2
+three = one + two
+print(three)
+
+hello = "Hello"
+world = "World"
+
+mix = hello+ " " + world
+print(f"Done {mix}")
---
+++ str.py
@@ -0,0 +1,5 @@
+# String can be defined with single and double quotes
+my_str = "Double quote's sheets"
+print(my_str)
+single = 'Single quote string'
+print(f"{single}")
