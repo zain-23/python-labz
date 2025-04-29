ebfa0bfafcb80fce41017efdd8f76ee35b0b73ee
zain-23 <zaingit780@gmail.com>
2025-04-29 16:33:20 +0500

    Add implementation for averaging two float numbers

---
+++ basic/input.py
@@ -0,0 +1,9 @@
+# username = input("Enter your name: ")
+# print(f"USERNAME: {username}")
+# number1 = int(input("Enter first number: "))
+# number2 = int(input("Enter second number: "))
+# print(number1 + number2)
+
+number1 = float(input("Enter first number: "))
+number2 = float(input("Enter second number: "))
+print((number1 + number2) / 2)
