f5cbdc8ae31602d62d00e6ce5a84cf62241ecbe5
zain-23 <zaingit780@gmail.com>
2025-04-30 16:29:35 +0500

    Refactor slicing.py to uncomment and organize string manipulation examples

--- condition/slicing.py
+++ condition/slicing.py
@@ -1,2 +1,9 @@
 str = "Hello World"
-print(str[0:5])
+# print(str[0:5])
+
+capitalize = "hello zain"
+capitalize = capitalize.capitalize()
+# print(capitalize)
+
+test = "My name is zain, i am from pakistan, developer at kodexo"
+print(test.count("a"))
