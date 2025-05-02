e2c985efa995e7efe8a180d34b830a167ff48d49
zain-23 <zaingit780@gmail.com>
2025-05-02 18:52:24 +0500

    Add initial implementations for for loop examples and multiplication table

---
+++ loops/forLoop.py
@@ -0,0 +1,21 @@
+# array = [10,12,13,14,15,16,17,18,19,20]
+
+# for el in array:
+#     if(el == 16):
+#         break
+#     print(el)
+# else:
+#     print("Loop completed")
+
+# for i in range(0, 11, 1):
+#     if(i % 2 == 0):
+#         print(f"{i} is even")
+#     else:
+#         print(f"{i} is odd")
+
+# sum of natural numbers
+n = 5
+sum = 0
+for i in range(1,n+1):
+    sum += i
+print(f"Sum of first {n} natural numbers is {sum}")
---
+++ loops/loops.py
@@ -0,0 +1,12 @@
+# count = 0
+# while count < 5:
+#     count += 1
+#     print(f"Hello {count}")
+
+# Multiplication table of 5
+num = 10
+length = 1
+while length <= 10:
+    print(f"{num} x {length} = {num * length}")
+    length += 1
+    
