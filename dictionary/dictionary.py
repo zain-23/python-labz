f7eee0324a125609714c6cfd4fd9f8329f53634d
zain-23 <zaingit780@gmail.com>
2025-05-02 14:38:50 +0500

    Add initial implementation of dictionary with basic info and city update

---
+++ dictionary/dictionary.py
@@ -0,0 +1,11 @@
+info = {
+    "name": "dictionary",
+    "age": 18,
+    "is_adult": True,
+    "hobbies": ["reading", "gaming", "coding"],
+}
+# info["name"] = "ZAIN"
+# print(info.update())
+# print(info["name2"]) # KeyError: 'name2'
+info.update({"city":"Karachi"}) # None
+print(info)
